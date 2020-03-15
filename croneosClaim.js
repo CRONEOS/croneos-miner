6808ba6678fe03696dfd2151dd5f906e49beb464
John Heeter <john@boid.com>
2020-03-15 07:26:17 -0500

    fix claim script

--- croneosClaim.js
+++ croneosClaim.js
@@ -1,30 +1,43 @@
 const CONF = require('./miner_config')
-const {api,rpc,boidjs} = require('../eosjs')([CONF.permissionPrivateKey])
+// const {api,rpc,boidjs} = require('../eosjs')([CONF.permissionPrivateKey])
 
-function parseReward(reward){
+const { Api, JsonRpc, RpcError, Serialize } = require('@jafri/eosjs2')
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig')
+const fetch = require('node-fetch')
+const { TextEncoder, TextDecoder } = require('util')
+const signatureProvider = new JsSignatureProvider([CONF.permissionPrivateKey])
+const rpc = new JsonRpc(CONF.rpc_nodes, { fetch })
+const api = new Api({
+  rpc,
+  signatureProvider,
+  textDecoder: new TextDecoder(),
+  textEncoder: new TextEncoder()
+})
+
+function parseReward (reward) {
   if(!reward) return;
   let [amount_p, sym] = reward.split(' ');
-  let precision = amount_p.includes(".") ? amount_p.split('.')[1].length-1 : 0;
-  let amount = amount_p.substring(0, amount_p.length - 1);//remove excess decimal
-  let res= {}
+  let precision = amount_p.includes(".") ? amount_p.split('.')[1].length - 1 : 0;
+  let amount = amount_p.substring(0, amount_p.length - 1); //remove excess decimal
+  let res = {}
   res.asset = `${amount} ${sym}`;
   res.can_withdraw = !!Number(amount);
   return res;
 }
 
 
-async function init(){
+async function init () {
   try {
     console.log('Querying for croneos profits...')
     const rewards = (await rpc.get_table_rows({
-      code:CONF.croneos_contract,
-      table:"rewards",
-      scope:CONF.miner_account
+      code: CONF.croneos_contract,
+      table: "rewards",
+      scope: CONF.miner_account
     })).rows
-    for (reward of rewards) {
-      console.log('Claiming Profit:',reward)
+    for(reward of rewards) {
+      console.log('Claiming Profit:', reward)
       const parsed = parseReward(reward.adj_p_balance)
-      if (!parsed.can_withdraw || parseFloat(parsed.asset) === 0) return console.log('Nothing to claim') 
+      if(!parsed.can_withdraw || parseFloat(parsed.asset) === 0) return console.log('Nothing to claim')
       const actions = [
         {
           account: CONF.croneos_contract,
@@ -36,13 +49,16 @@ async function init(){
             }
           ],
           data: {
-            amount:parsed.asset,
+            amount: parsed.asset,
             miner: CONF.miner_account
           }
         }
       ]
       console.log(actions)
-      const result = await api.transact({actions},boidjs.tx.tapos)
+      const result = await api.transact({ actions }, {
+        blocksBehind: 3,
+        expireSeconds: 300
+      })
       console.log(result)
     }
     // const result = 
